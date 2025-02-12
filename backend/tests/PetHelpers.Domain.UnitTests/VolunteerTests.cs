ddaedf6a8b50a63300a5a9223477b142f76bf22d
taq <bububuiok@mail.ru>
2025-02-12 21:16:07 +0500

    Add MovePet() Tests

--- backend/tests/PetHelpers.Domain.UnitTests/VolunteerTests.cs
+++ backend/tests/PetHelpers.Domain.UnitTests/VolunteerTests.cs
@@ -135,4 +135,205 @@ public class VolunteerTests
         addedPetResult.IsSuccess.Should().BeTrue();
         addedPetResult.Value.SerialNumber.Should().Be(serialNumber);
     }
+
+    [Fact]
+    public void MovePet_Right_ReturnsCorrectResults()
+    {
+        // Arrange
+        const int petsCount = 10;
+
+        var yearsOfExperience = 0;
+        var description = Description.Create("test").Value;
+        var email = Email.Create("test@test.com").Value;
+        var fullName = FullName.Create("test", "test").Value;
+        var phoneNumber = PhoneNumber.Create("+79000323565").Value;
+
+        var volunteer = Volunteer.Volunteer.Create(
+            yearsOfExperience,
+            description,
+            email,
+            fullName,
+            phoneNumber).Value;
+
+        var height = 1;
+        var weight = 1;
+        var isCastrated = true;
+        var isVaccinated = true;
+        var birthDate = DateOnly.MinValue;
+        var petName = PetName.Create("test").Value;
+        var color = Color.Red;
+        var healthInfo = HealthInfo.Create("test").Value;
+        var location = Location.Create("test", "test", "test").Value;
+        var helpStatus = Status.NeedsHelp;
+        var speciesAndBreed = new SpeciesAndBreed(Guid.NewGuid(), Guid.NewGuid());
+
+        var pets = Enumerable.Range(1, petsCount).Select(_ => Pet.Create(
+            height,
+            weight,
+            isCastrated,
+            isVaccinated,
+            birthDate,
+            petName,
+            description,
+            color,
+            healthInfo,
+            location,
+            helpStatus,
+            speciesAndBreed,
+            phoneNumber).Value).ToList();
+
+        foreach (var p in pets)
+            volunteer.AddPet(p);
+
+        var petToMove1 = volunteer.OwnedPets[1];
+        var petToMove2 = volunteer.OwnedPets[7];
+
+        // Act
+        var result = volunteer.MovePet(petToMove1.SerialNumber, petToMove2.SerialNumber);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        volunteer.OwnedPets[7].Should().Be(pets[1]);
+
+        for (int i = 1; i < 7; i++)
+        {
+            volunteer.OwnedPets[i].Should().Be(pets[i + 1]);
+        }
+
+        for (int i = 0; i < pets.Count; i++)
+        {
+            volunteer.OwnedPets[i].SerialNumber.Value.Should().Be(i + 1);
+        }
+    }
+
+    [Fact]
+    public void MovePet_Left_ReturnsCorrectResults()
+    {
+        // Arrange
+        const int petsCount = 10;
+
+        var yearsOfExperience = 0;
+        var description = Description.Create("test").Value;
+        var email = Email.Create("test@test.com").Value;
+        var fullName = FullName.Create("test", "test").Value;
+        var phoneNumber = PhoneNumber.Create("+79000323565").Value;
+
+        var volunteer = Volunteer.Volunteer.Create(
+            yearsOfExperience,
+            description,
+            email,
+            fullName,
+            phoneNumber).Value;
+
+        var height = 1;
+        var weight = 1;
+        var isCastrated = true;
+        var isVaccinated = true;
+        var birthDate = DateOnly.MinValue;
+        var petName = PetName.Create("test").Value;
+        var color = Color.Red;
+        var healthInfo = HealthInfo.Create("test").Value;
+        var location = Location.Create("test", "test", "test").Value;
+        var helpStatus = Status.NeedsHelp;
+        var speciesAndBreed = new SpeciesAndBreed(Guid.NewGuid(), Guid.NewGuid());
+
+        var pets = Enumerable.Range(1, petsCount).Select(_ => Pet.Create(
+            height,
+            weight,
+            isCastrated,
+            isVaccinated,
+            birthDate,
+            petName,
+            description,
+            color,
+            healthInfo,
+            location,
+            helpStatus,
+            speciesAndBreed,
+            phoneNumber).Value).ToList();
+
+        foreach (var p in pets)
+            volunteer.AddPet(p);
+
+        var petToMove1 = volunteer.OwnedPets[7];
+        var petToMove2 = volunteer.OwnedPets[1];
+
+        // Act
+        var result = volunteer.MovePet(petToMove1.SerialNumber, petToMove2.SerialNumber);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        volunteer.OwnedPets[1].Should().Be(pets[7]);
+
+        for (int i = 2; i <= 7; i++)
+        {
+            volunteer.OwnedPets[i].Should().Be(pets[i - 1]);
+        }
+
+        for (int i = 0; i < pets.Count; i++)
+        {
+            volunteer.OwnedPets[i].SerialNumber.Value.Should().Be(i + 1);
+        }
+    }
+
+    [Fact]
+    public void MovePet_WithIncorrectInput_ReturnsErrorResult()
+    {
+        // Arrange
+        const int petsCount = 10;
+
+        var yearsOfExperience = 0;
+        var description = Description.Create("test").Value;
+        var email = Email.Create("test@test.com").Value;
+        var fullName = FullName.Create("test", "test").Value;
+        var phoneNumber = PhoneNumber.Create("+79000323565").Value;
+
+        var volunteer = Volunteer.Volunteer.Create(
+            yearsOfExperience,
+            description,
+            email,
+            fullName,
+            phoneNumber).Value;
+
+        var height = 1;
+        var weight = 1;
+        var isCastrated = true;
+        var isVaccinated = true;
+        var birthDate = DateOnly.MinValue;
+        var petName = PetName.Create("test").Value;
+        var color = Color.Red;
+        var healthInfo = HealthInfo.Create("test").Value;
+        var location = Location.Create("test", "test", "test").Value;
+        var helpStatus = Status.NeedsHelp;
+        var speciesAndBreed = new SpeciesAndBreed(Guid.NewGuid(), Guid.NewGuid());
+
+        var pets = Enumerable.Range(1, petsCount).Select(_ => Pet.Create(
+            height,
+            weight,
+            isCastrated,
+            isVaccinated,
+            birthDate,
+            petName,
+            description,
+            color,
+            healthInfo,
+            location,
+            helpStatus,
+            speciesAndBreed,
+            phoneNumber).Value).ToList();
+
+        foreach (var p in pets)
+            volunteer.AddPet(p);
+
+        var petToMove = volunteer.OwnedPets[0];
+        var incorrectSerialNumber = SerialNumber.Create(pets.Count + 1).Value;
+
+        // Act
+        var incorrectTargetResult = volunteer.MovePet(petToMove.SerialNumber, incorrectSerialNumber);
+        var incorrectCurrentResult = volunteer.MovePet(incorrectSerialNumber, petToMove.SerialNumber);
+
+        // Assert
+        incorrectTargetResult.IsFailure.Should().BeTrue();
+        incorrectCurrentResult.IsFailure.Should().BeTrue();
+    }
 }
