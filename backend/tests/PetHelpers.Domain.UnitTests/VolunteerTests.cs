5de130ae469f91e2c521af8a84dfdc6cfd963b1a
taq <bububuiok@mail.ru>
2025-04-21 15:56:15 +0500

    Some tests refactoring

--- backend/tests/PetHelpers.Domain.UnitTests/VolunteerTests.cs
+++ backend/tests/PetHelpers.Domain.UnitTests/VolunteerTests.cs
@@ -60,12 +60,11 @@ public class VolunteerTests
         addedPetResult.Value.Position.Should().Be(Position.First);
     }
 
-    [Fact]
-    public void AddPet_WithExistingPets_ReturnsSuccessfulResult()
+    [Theory]
+    [InlineData(5)]
+    public void AddPet_WithExistingPets_ReturnsSuccessfulResult(int petsCount)
     {
         // Arrange
-        const int petsCount = 5;
-
         var yearsOfExperience = 0;
         var description = Description.Create("test").Value;
         var email = Email.Create("test@test.com").Value;
@@ -136,12 +135,13 @@ public class VolunteerTests
         addedPetResult.Value.Position.Should().Be(position);
     }
 
-    [Fact]
-    public void MovePet_Right_ReturnsCorrectResults()
+    [Theory]
+    [InlineData(1, 7, 10)]
+    [InlineData(0, 9, 10)]
+    [InlineData(0, 1, 2)]
+    public void MovePet_Right_ReturnsCorrectResults(int petToMoveId, int targetPetId, int petsCount)
     {
         // Arrange
-        const int petsCount = 10;
-
         var yearsOfExperience = 0;
         var description = Description.Create("test").Value;
         var email = Email.Create("test@test.com").Value;
@@ -185,17 +185,17 @@ public class VolunteerTests
         foreach (var p in pets)
             volunteer.AddPet(p);
 
-        var petToMove1 = volunteer.OwnedPets[1];
-        var petToMove2 = volunteer.OwnedPets[7];
+        var petToMove = volunteer.OwnedPets[petToMoveId];
+        var targetPet = volunteer.OwnedPets[targetPetId];
 
         // Act
-        var result = volunteer.MovePet(petToMove1.Position, petToMove2.Position);
+        var result = volunteer.MovePet(petToMove.Position, targetPet.Position);
 
         // Assert
         result.IsSuccess.Should().BeTrue();
-        volunteer.OwnedPets[7].Should().Be(pets[1]);
+        volunteer.OwnedPets[targetPetId].Should().Be(pets[petToMoveId]);
 
-        for (int i = 1; i < 7; i++)
+        for (int i = petToMoveId; i < targetPetId; i++)
         {
             volunteer.OwnedPets[i].Should().Be(pets[i + 1]);
         }
@@ -206,12 +206,13 @@ public class VolunteerTests
         }
     }
 
-    [Fact]
-    public void MovePet_Left_ReturnsCorrectResults()
+    [Theory]
+    [InlineData(7, 1, 10)]
+    [InlineData(9, 0, 10)]
+    [InlineData(1, 0, 2)]
+    public void MovePet_Left_ReturnsCorrectResults(int petToMoveId, int targetPetId, int petsCount)
     {
         // Arrange
-        const int petsCount = 10;
-
         var yearsOfExperience = 0;
         var description = Description.Create("test").Value;
         var email = Email.Create("test@test.com").Value;
@@ -255,17 +256,17 @@ public class VolunteerTests
         foreach (var p in pets)
             volunteer.AddPet(p);
 
-        var petToMove1 = volunteer.OwnedPets[7];
-        var petToMove2 = volunteer.OwnedPets[1];
+        var petToMove = volunteer.OwnedPets[petToMoveId];
+        var targetPet = volunteer.OwnedPets[targetPetId];
 
         // Act
-        var result = volunteer.MovePet(petToMove1.Position, petToMove2.Position);
+        var result = volunteer.MovePet(petToMove.Position, targetPet.Position);
 
         // Assert
         result.IsSuccess.Should().BeTrue();
-        volunteer.OwnedPets[1].Should().Be(pets[7]);
+        volunteer.OwnedPets[targetPetId].Should().Be(pets[petToMoveId]);
 
-        for (int i = 2; i <= 7; i++)
+        for (int i = targetPetId + 1; i <= petToMoveId; i++)
         {
             volunteer.OwnedPets[i].Should().Be(pets[i - 1]);
         }
@@ -276,12 +277,11 @@ public class VolunteerTests
         }
     }
 
-    [Fact]
-    public void MovePet_WithIncorrectInput_ReturnsErrorResult()
+    [Theory]
+    [InlineData(10)]
+    public void MovePet_WithIncorrectInput_ReturnsErrorResult(int petsCount)
     {
         // Arrange
-        const int petsCount = 10;
-
         var yearsOfExperience = 0;
         var description = Description.Create("test").Value;
         var email = Email.Create("test@test.com").Value;
