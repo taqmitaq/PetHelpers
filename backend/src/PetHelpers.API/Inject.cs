18a86e2d766bf6dc53a4f37e5f027f757ba06fd4
taq <bububuiok@mail.ru>
2025-04-21 16:28:42 +0500

    Some refactoring of Program.cs

--- backend/src/PetHelpers.API/Inject.cs
+++ backend/src/PetHelpers.API/Inject.cs
@@ -1,4 +1,5 @@
 ﻿using Serilog;
+using Serilog.Events;
 
 namespace PetHelpers.API;
 
@@ -6,10 +7,30 @@ public static class Inject
 {
     public static IServiceCollection AddAPI(this IServiceCollection services)
     {
+        services.AddLogging();
         services.AddSerilog();
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static IServiceCollection AddLogging(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .WriteTo.Seq(configuration.GetConnectionString("Seq")
+                         ?? throw new ArgumentNullException("Seq"))
+            .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+            .CreateLogger();
+
+        services.AddSerilog();
 
         return services;
     }
